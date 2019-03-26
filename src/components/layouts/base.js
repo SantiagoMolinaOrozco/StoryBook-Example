fd85f3bdba4316862fbf7cebbbf2e4c59a7e9422
Santiago Molina Orozco <santi020k@gmail.com>
2019-03-26 00:13:54 -0500

    Refactor scaffolding

--- src/components/layouts/base.js
+++ src/components/layouts/base.js
@@ -1,5 +1,5 @@
 /**
- * Layout component that queries for data
+ * Base component that queries for data
  * with Gatsby's StaticQuery component
  *
  * See: https://www.gatsbyjs.org/docs/static-query/
@@ -9,10 +9,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
-import './layout.css'
+import Header from '../molecules/header'
+import './base.css'
 
-const Layout = ({ children }) => (
+const Base = ({ children }) => (
   <StaticQuery
     query={graphql`
       query SiteTitleQuery {
@@ -46,8 +46,8 @@ const Layout = ({ children }) => (
   />
 )
 
-Layout.propTypes = {
+Base.propTypes = {
   children: PropTypes.node.isRequired
 }
 
-export default Layout
+export default Base
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Base from '../components/layouts/base'
+import SEO from '../components/molecules/seo'
 
 const SecondPage = () => (
-  <Layout>
+  <Base>
     <SEO title='Page two' />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to='/'>Go back to the homepage</Link>
-  </Layout>
+  </Base>
 )
 
 export default SecondPage
