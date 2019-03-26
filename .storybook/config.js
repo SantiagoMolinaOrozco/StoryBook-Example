acab9b5c4cf0438091c92c348ad9a4ca86b42ca8
Santiago Molina Orozco <santi020k@gmail.com>
2019-03-25 23:58:30 -0500

    Add components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,41 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react'
+import { addDecorator } from '@storybook/react'
+import requireContext from 'require-context.macro'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
+import "../src/styles/global.css"
+ 
+const newViewports = {
+  kindleFire2: {
+    name: 'Kindle Fire 2',
+    styles: {
+      width: '600px',
+      height: '963px'
+    }
+  },
+  kindleFireHD: {
+    name: 'Kindle Fire HD',
+    styles: {
+      width: '533px',
+      height: '801px'
+    }
+  }
 }
 
-configure(loadStories, module);
+addParameters({
+  viewport: {
+    defaultViewport: 'iphone6',
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+      ...newViewports
+    }
+  }
+})
+
+const req = requireContext('../src/components', true, /\.stories\.js$/)
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename))
+}
+
+configure(loadStories, module)
---
+++ src/pages/todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import store from '../lib/redux'
+
+import InboxScreen from '../components/InboxScreen'
+
+const TodoPage = () => (
+  <Provider store={store}>
+    <InboxScreen />
+  </Provider>
+)
+
+export default TodoPage
